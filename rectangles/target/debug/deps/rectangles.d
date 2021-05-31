160fdc51c08f6e1f2e11e5f32ca34f8c491ca9e5
qyqyq <qyq9792@Gmail.com>
2021-05-31 15:13:03 +0800

    Add files via upload

---
+++ rectangles/target/debug/deps/rectangles.d
@@ -0,0 +1,5 @@
+E:\20Spring\Rust\rectangles\target\debug\deps\rectangles.exe: src\main.rs
+
+E:\20Spring\Rust\rectangles\target\debug\deps\rectangles.d: src\main.rs
+
+src\main.rs:
---
+++ rectangles/target/debug/rectangles.d
@@ -0,0 +1,1 @@
+E:\20Spring\Rust\rectangles\target\debug\rectangles.exe: E:\20Spring\Rust\rectangles\src\main.rs
